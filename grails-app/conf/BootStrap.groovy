874ea65b9decf97268a2b3c335910e642f60d616
Chris Latimer <chris@chrislatimer.com>
2014-03-15 20:57:43 -0400

    Added random data seeding on startup

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,43 @@
-import grails.converters.JSON
+import grails.util.Environment
 import phone.rest.Manufacturer
 import phone.rest.Phone
+import phone.rest.ProductVariation
 
 class BootStrap {
 
     def init = { servletContext ->
+        if(Environment.TEST != Environment.current) {
+            Random random = new Random()
+            def phonePrefix = ["Mega", "Giga", "Xtreme", "Galactic", "Nano", "Plutonium", "Crypto", "Edge"]
+            def phoneMiddle = ["Galaxy", "Universe", "Element", "Photon", "Compound", "xPhone", "Communicator", "L4", "X4", "AERO", "Spec", "Lens"]
+            def letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+            def numbers = ["1","2","3","4","5"]
+            def variations = ["16GB", "32GB", "64GB", "128GB"]
+            def manufacturers = ["Samsang", "Sweedia", "Fruit", "Googly", "XTC", "Sonny", "Blueberry"]
 
+            manufacturers.each {
+                Manufacturer man = new Manufacturer(name: it)
+                random.nextInt(40).times {
+                    Phone p = new Phone()
+                    p.name = "${phonePrefix[random.nextInt(phonePrefix.size())]} ${phoneMiddle[random.nextInt(phonePrefix.size())]} " +
+                             "${letters[random.nextInt(letters.size())]}${numbers[random.nextInt(numbers.size())]}"
+                    def price = random.nextInt(500)
+                    def first = true
+                    variations[0..random.nextInt(4)].each {
+                        ProductVariation v = new ProductVariation()
+                        v.label = it
+                        v.salePrice = price
+                        v.listPrice = price
+                        v.baseVariation = first
+                        first = false
+                        price += 100
+                        p.addToProductVariations(v)
+                    }
+                    man.addToProducts(p)
+                }
+                man.save(failOnError: true)
+            }
+        }
     }
 
     def destroy = {
